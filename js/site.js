0b5dc52ea00cc247a0fe11cac0418b86ce2c1cf9
Ryan Penley <ryanpenley@gmail.com>
2023-01-26 17:03:49 -0500

    fixed message

--- js/site.js
+++ js/site.js
@@ -2,27 +2,69 @@
 function getValues() {
     let inputString = document.getElementById('userString').value;
 
-    let checkPalin = checkForPalindrome(inputString);
+    let check = checkForPalindrome(inputString);
 
-    displayResults(checkPalin, inputString);
+    displayResults(check, inputString);
 }
 
 // logic function
 function checkForPalindrome(userString) {
 
-    for(let i = 0; i < userString.length/2; i++) {
-        if(userString[i] != userString[userString.length - 1 - i]) {
-            return textContent = "Uh-oh! This is not a Palindrome!";
-        }
-    }
+    userString = userString.toLowerCase();
 
-    return textContent = "Way to go! This is a Palindrome!";
+    const regex = /[^a-z0-0]/gi;
+    userString = userString.replace(regex, "");
+
+    for(let i = 0; i < userString.length/2; i++) {
+        if(userString[i] != userString[userString.length - 1 - i])
+    {
+        // if not a palindrome
+        return false;
+    } else {
+        // not a palindrome
+        return true;
+    }
+    }
 }
 
-// view function
-function displayResults(checkPalin) {
+function reverseString(string) {
+    let reversedString = '';
 
-    document.getElementById('results').textContent = checkPalin;
-    document.getElementById('alert').classList.remove('invisible');
+    for (let i = string.length -1; i >= 0; i--) {
+        reversedString += string[i];
+    }
+
+    return reversedString;
+}
+
+
+
+
+// view function
+function displayResults(revString, userString) {
+
+    let alert = document.getElementById('alert');
+
+    let reversedString = reverseString(userString);
+
+    let resultsElement = document.getElementById('results');
+
+    let messageElement = document.getElementById('message');
+
+    //reset alert box
+    alert.classList.remove('alert-danger');
+    alert.classList.remove('alert-success');
+
+    if (revString) {
+        alert.classList.add('alert-success');
+        resultsElement.textContent = "Congrats! It's a palindrome!";
+        messageElement.textContent  = "Your phrased reversed: " + reversedString;
+    } else {
+        alert.classList.add('alert-danger');
+        resultsElement.textContent = "Oh no! It's not a palindrome!";
+        messageElement.textContent  = "Your phrased reversed: " + reversedString;
+    }
+
+    alert.classList.remove('invisible');
 
 }
